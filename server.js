6d1fbe2f2b6bb7442181c7cd6241b82e2597af96
jensadev <jensandreasson77@gmail.com>
2025-09-01 14:44:28 +0200

    lektion v35

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from "express"
-
+// let, var
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send("<h1>Hello te23 väklmona!<h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.json({
+        "message": "Hatisk textbox"
+    })
 })
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+  console.log("Server is running on http://localhost:3000")
 })
