a9f97b68421d62aae64014aef1d6e2e43ee77432
jensadev <jensandreasson77@gmail.com>
2025-09-24 10:40:31 +0200

    Refactor server structure by moving route handlers to a separate index file

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.render("index.njk", {
+        title: "Vår första dynamiska sida",
+        message: "Med Nunjucks skapar vi magi!"
+    })
+})
+
+router.get("/about", (req, res) => {
+    res.render("about.njk", {
+        title: "Om oss",
+        message: "Detta är ett skolarbete av Jens."
+    })
+})
+
+router.get("/greeting", (req, res) => {
+    console.log(req.query)
+    // res.send(`Hejsan ${req.query.name}, ${req.query.message}`)
+    res.render("greeting.njk", {
+        title: "Hälsningssida",
+        name: req.query.name,
+        message: req.query.message
+    })
+})
+
+export default router
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express"
 import nunjucks from "nunjucks"
 import morgan from "morgan"
 
+import indexRouter from "./routes/index.js"
+
 const app = express()
 
 app.use(morgan("dev"))
@@ -12,29 +14,7 @@ nunjucks.configure("views", {
     express: app
 })
 
-app.get("/", (req, res) => {
-    res.render("index.njk", {
-        title: "Vår första dynamiska sida",
-        message: "Med Nunjucks skapar vi magi!"
-    })
-})
-
-app.get("/about", (req, res) => {
-    res.render("about.njk", {
-        title: "Om oss",
-        message: "Detta är ett skolarbete av Jens."
-    })
-})
-
-app.get("/greeting", (req, res) => {
-    console.log(req.query)
-    // res.send(`Hejsan ${req.query.name}, ${req.query.message}`)
-    res.render("greeting.njk", {
-        title: "Hälsningssida",
-        name: req.query.name,
-        message: req.query.message
-    })
-})
+app.use("/", indexRouter)
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000")
